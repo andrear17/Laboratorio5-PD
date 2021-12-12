0be85b28f9750054517c0196678c3a7dea26505f
andrear17 <andrea.rivas@galileo.edu>
2021-12-11 23:51:58 -0600

    Update pipeline_predict.py

--- pipeline_predict.py
+++ pipeline_predict.py
@@ -11,5 +11,5 @@ def predict(X):
     predicts = titanic_model.predict(X)
     return predicts[0]
 
-#X_test = pd.json_normalize({"PassengerId":1,"Pclass":1,"Name": "Mike Dallas","Sex": "male","Age": 22,"SibSp": 0,"Parch": 0,"Ticket": 649,"Fare": 7.25,"Cabin": "B14","Embarked":"S"})
+#X_test = pd.json_normalize({"PassengerId":pd.NA,"Pclass":1,"Name": "Mike Dallas","Sex": "male","Age": 22,"SibSp": 0,"Parch": 0,"Ticket": 649,"Fare": 7.25,"Cabin": "B14","Embarked":"S"})
 #print('Resultado', predict(X_test))
